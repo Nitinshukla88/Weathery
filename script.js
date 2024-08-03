9e554e6a01abee99b8424f9adf7822f9278f0be6
Nitin shukla <nitin.shukla12004@gmail.com>
2024-08-03 16:48:39 +0530

    Started working on styling and the added the maincontainer

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 let button = document.getElementsByTagName("button");
 let form = document.getElementsByTagName("input");
 
-let city = function x() {
-button[0].addEventListener("click", function(){
-    return form[0].value;
-});
-}
 
-console.log(city());
-
-console.log(city);
-
-const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Shimla%20India';
 const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'd669bc9c79mshb9ef3109ab02809p17c2bcjsn80263c2d2104',
-		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "d669bc9c79mshb9ef3109ab02809p17c2bcjsn80263c2d2104",
+    "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+  },
 };
 
-async function get(){
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        const data = JSON.parse(result);
-        console.log(data);
-        console.log(data.current.temp_c);
-    } catch (error) {
-        console.error(error);
-    }
+button[0].addEventListener("click", function () {
+    let city = form[0].value;
+    const url =
+    `https://weatherapi-com.p.rapidapi.com/current.json?q=${city}%20`;
+    get(url);
+  });
+
+async function get(url) {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    const data = JSON.parse(result);
+    console.log(data);
+    console.log(data.current.temp_c);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-get();
+
+
 
